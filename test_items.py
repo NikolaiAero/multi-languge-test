ad9645dae3e70e24a2787bf8d7c701c80705e29a
Николай Плющенков <plushenkov@atsaero.ru>
2022-08-22 11:44:47 +0200

    edit method name

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestMainPage():
 
-    def test_add_bookmark_to_cart(self,browser):
+    def test_setup_language(self,browser):
         browser.get(link)
         button = browser.find_elements(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
         button_len = len(button)
