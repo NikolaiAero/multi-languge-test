0a381e7efb76c57ba1cbfa55508d23aae6cac186
Николай Плющенков <plushenkov@atsaero.ru>
2022-08-22 11:36:03 +0200

    edit sleep

--- test_items.py
+++ test_items.py
@@ -6,9 +6,9 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestMainPage():
+
     def test_add_bookmark_to_cart(self,browser):
         browser.get(link)
-        # time.sleep(3)
         button = browser.find_elements(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
         button_len = len(button)
         assert button_len >= 1 , 'BUTTON NOT FOUND'
